6067c16932f8516571fcad404a23becfdcf7fab7
prachi8862 <pjha45032@gmail.com>
2024-03-13 09:21:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import BtnComponent from './component/BtnComponent';
+import DisplayComponent from './component/DisplayComponent';
+
 
 function App() {
+  const [time, setTime] = useState({ ms: 0, s: 0, m: 0, h: 0 });
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));
+  };
+  var updatedMs = time.ms,
+    updatedS = time.s,
+    upadatedM = time.m,
+    upadtedH = time.h;
+  const run = () => {
+    if (upadatedM === 60) {
+      upadtedH++;
+      upadatedM = 0;
+    }
+    if ( updatedS === 60) {
+      upadatedM++;
+      updatedS = 0;
+    }
+    if (updatedMs === 60) {
+      updatedS++;
+      updatedMs = 0;
+    }
+    updatedMs++;
+    return setTime({ ms: updatedMs, s: updatedS, m: upadatedM, h: upadtedH });
+  };
+  const stop = () => {
+    clearInterval(interv);
+    setStatus(2);
+  };
+  const reset = () => {
+    clearInterval(interv);
+    setStatus(0);
+    setTime({ ms: 0, s: 0, m: 0, h: 0 });
+  };
+  const resume = () => start();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-section">
+      <div className="clock-holder">
+        <div className="stopwatch">
+          <DisplayComponent time={time} />
+          <BtnComponent
+            status={status}
+            resume={resume}
+            reset={reset}
+            stop={stop}
+            start={start}
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/component/BtnComponent.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function BtnComponent(props) {
+  return (
+  <div>
+     {(props.status === 0)?
+        <button className='stopwatch-btn stopwatch-btn-gre'onClick={props.start}>start</button>
+     : ""}
+     {(props.status === 1)?
+       <div>
+        <button className='stopwatch-btn stopwatch-btn-red'onClick={props.stop}>stop</button>
+        <button className='stopwatch-btn stopwatch-btn-yel'onClick={props.reset}>reset</button>
+
+     </div>
+     : ""}
+      {(props.status === 2)?
+       <div>
+            <button className='stopwatch-btn stopwatch-btn-gre'onClick={props.resume}>resume</button>
+        <button className='stopwatch-btn stopwatch-btn-yel'onClick={props.reset}>reset</button>
+     </div>
+     : ""}
+        
+
+  </div>
+  );
+}
+
+export default BtnComponent;
